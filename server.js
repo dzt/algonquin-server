154c6883d74e188bc628980dd829bd7b136c299e
Peter Soboyejo <thepcmrtim@gmail.com>
2017-08-26 10:38:51 -0400

    Damn it Heroku

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
-var port = 3000 || process.env.PORT;
+var port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
